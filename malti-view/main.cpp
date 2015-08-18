25c9143a0d5399990bfb599d7902875f244e9f62
AFJay <jeremya11z@gmail.com>
2015-08-18 00:28:21 -0500

    Added configuration file and command-line option to set home page

--- malti-view/main.cpp
+++ malti-view/main.cpp
@@ -11,31 +11,43 @@ int main(int argc, char *argv[])
 
 
     QCommandLineParser parser;
-       parser.setApplicationDescription(QCoreApplication::translate("main",
+    parser.setApplicationDescription(QCoreApplication::translate("main",
             "Light weight minimum browser."));
-       parser.addHelpOption();
-       parser.addVersionOption();
+    parser.addHelpOption();
+    parser.addVersionOption();
 
-       parser.addPositionalArgument(QStringLiteral("url"),
+    parser.addPositionalArgument(QStringLiteral("url"),
             QCoreApplication::translate("main","The URL to load at start up"));
 
-       parser.process(a);
+    QCommandLineOption setHomeOption(QStringList() << "s" << "sethome",
+                       QCoreApplication::translate("main", "Specify home page"),
+                       QCoreApplication::translate("main", "URL"));
+    parser.addOption(setHomeOption);
+
+
+    parser.process(a);
 
     const QStringList args = parser.positionalArguments();
+
+    //Access config.ini directly
+    QSettings settings(QString("malti-view.conf"),
+              QSettings::IniFormat);
     QUrl url;
 
-   if (args.size() > 1) {
+    if(parser.isSet(setHomeOption))
+       settings.setValue("BrowserOptions/HomePage", parser.value(setHomeOption));
+
+    if (args.size() > 1) {
        fprintf(stderr, "%s\n", qPrintable(QCoreApplication::translate("main",
             "Error: Must specify one URL argument.")));
        parser.showHelp(1);
        return -1;
     }
-   else if (args.size() == 1)
+    else if (args.size() == 1)
        url = QUrl::fromUserInput(args.at(0));
-   else
-   {
-       //ToDo Add config file for home page
-   }
+    else
+       url = QUrl::fromUserInput(settings.value("BrowserOptions/HomePage",
+                "hackaday.io/project/5081-malti").toString());
 
    if (!url.isValid()) {
        fprintf(stderr, "%s\n", qPrintable(QCoreApplication::translate("main",
